65d1f74510e9caabf638dc8de109d072436b5d4d
Yiwen Gong <gong32@ecegrid-thin1.ecn.purdue.edu>
2014-08-30 15:53:05 -0400

    before changing the last function

--- PA01/answer01.c
+++ PA01/answer01.c
@@ -2,26 +2,78 @@
 #include "answer01.h"
 
 int arraySum(int * array, int len)
-{
-    return 0;
+{   
+    int sum = 0;
+    int ind;
+    for(ind =0;ind<len;ind++)
+    {
+      sum = sum +array[ind];
+    }
+    return sum;
 }
 
 int arrayCountNegative(int * array, int len)
 {
-    return 0;
+    int count = 0;
+    int ind;
+    for(ind = 0; ind < len; ind++)
+    {
+      if(array[ind] < 0)
+      {
+        count++;
+      }
+    return count;
 }
 
 int arrayIsIncreasing(int * array, int len)
-{    
-    return 0;
+{   
+    int ind;
+    int incre = 1;
+    for(ind = 0; ind < len - 1; ind++)
+    {
+      if(array[ind] > array[ind + 1])
+      {
+         incre = 0;
+      }
+
+    return incre;
 }
 
 int arrayIndexRFind(int needle, const int * haystack, int len)
 {
-    return 0;
+    int ind;
+    int pos;
+    for(ind = 0;ind < len;ind++)
+    {
+      if(haystack[ind] == needle)
+      {
+        pos = ind;
+      }
+      if(haystack[ind] != needle && ind == len - 1)
+      {
+        pos = -1;
+      }
+    return pos;
 }
 
 int arrayFindSmallest(int * array, int len)
 {
-    return 0;
+    int ind;
+    int small = array[0];
+    int pos;
+    
+    for(ind = 0; ind < len; ind++)
+    {
+      if(small < array[ind+1])
+      {
+        pos = ind;
+        small = array[ind];
+      }
+    }
+    if(len==0)
+    {
+      small =0;
+    }
+
+    return small;
 }

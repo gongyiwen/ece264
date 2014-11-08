da3a53376469d2f59251ba1af6432923c9473506
Yiwen Gong <gong32@ecegrid-thin5.ecn.purdue.edu>
2014-11-08 17:07:45 -0500

    first attempt

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -36,45 +36,117 @@ BusinessNode * tree_insert(BusinessNode * node, BusinessNode * root)
   return root;
 }
 
+char * * explode(const char * str, const char * delims)
+{  
+  int ind;
+  int N = 0;
+  int arrInd = 0; // this is the next position where we'll create a string
+  int last = 0; // 1 + the last index we saw a delimiter. Init to 0.
+  for(ind = 0;ind<strlen(str);ind++)
+  {
+    if(strchr(delims, str[ind]) != NULL)
+    {
+      N++;
+    }
+  }
+  int arrLen = N+1; //if there is one delim, there should be two arrays in the string.
+  char * * strArr = malloc((arrLen) * sizeof(char *));//create the return array
+  if(N != 0)
+  {
+    while(arrInd < N)
+    {
+      for(ind = 0; ind <strlen(str) ; ind++)
+      {
+	if(strchr(delims, str[ind]) != NULL)
+	{
+	  strArr[arrInd] = malloc(sizeof(char) * (ind-last+1));
+	  memcpy(strArr[arrInd],str+last,ind-last);
+	  strArr[arrInd][ind-last] = '\0';
+	  last = ind + 1;
+	  arrInd++;
+	}
+      }
+    }
+    strArr[arrInd] = malloc(sizeof(char) * (ind-last+1));//To create the array for the last word
+    memcpy(strArr[arrInd],str+last,ind-last);
+    strArr[arrInd][ind-last] = '\0';
+  }
+  else
+  {
+    strArr[arrInd] = malloc(sizeof(char) * (ind-last+1));
+    memcpy(strArr[arrInd],str+last,ind-last);
+    strArr[arrInd][ind-last] = '\0';
+  }
+  return strArr;
+}
 
-/* Parse a .tsv file line by line, create a BusinessNode for each entry, and
- * enter that node into a new BST. Return a pointer to the root of the BST.
- *
- * The explode(...) function from PA03 may be useful for breaking up a lines 
- * into seperate fields. 
- */
 BusinessNode * load_tree_from_file(char * filename)
-
-
-/* Search a BusinessNode BST for the node with the name 'name'. Returns
- * pointer to the node with a match.
- *
- * If there is no match, return NULL.
- */
+{
+  FILE * fptr = fopen(filename,'r');
+  BussinessNode * root = NULL;
+  while(!foef(fptr))
+  {
+    char * ch;
+    int ind = 0;
+   do
+   {
+     ch[ind] = fgetc(fptr);
+     ind++;
+   }while(ch != '\n');
+   char **strArr = explode(ch,'\t');
+   BussinessNode * node = create_node(strArr[0],strArr[1],strArr[2]);
+   root = tree_insert(node, root);
+  }
+  return root;
+}
+     
 BusinessNode * tree_search_name(char * name, BusinessNode * root)
+{
+  if(root == NULL)
+  {
+    return NULL;
+  }
+  if(name == (root -> name))
+  {
+    return root;
+  }
+  if(name <= (root -> name))
+  {
+    return tree_search_name(name, root -> left);
+  }
+  return tree_search_name(name, root -> right);
+}
 
-/* Print out a single node: name, address, and stars
- * The format can be similar to this:
- *
- * Country Cafe
- * ============
- * Stars:
- *    3.5
- * Address:
- *    1030 Emerald Ter, Sun Prairie, WI 53590
- *
- * This function is not graded, but it could come in very handful while
- * debugging this assignment.
- */
+
 void print_node(BusinessNode * node)
+{
+  if(node == NULL)
+  {
+    printf("The node is empty.");
+  }
+  printf("rate:%s \nname: %s \naddress: %s \n",node -> stars, node -> name, node -> address);
+}
 
-/* Print the entire tree, starting from the root. Like the print_node(...)
- * function, this is not graded.
- */
 void print_tree(BusinessNode * tree)
+{
+  if(tree == NULL)
+  {
+    return;
+  }
+  printf("rate:%s name: %s address: %s\n",tree -> stars, tree -> name, tree -> address);
+  print_tree(printPreorder(tree -> left);
+  print_tree(printPreorder(tree -> right);
+}
 
-/* Deallocate all of the memory used by a BusinessNode BST, without memory
- * leaks.
- */
+
 void destroy_tree(BusinessNode * root)
+{
+  if(root == NULL)
+  {
+    return;
+  }
+  destroy_tree(root -> left);
+  destory_tree(root -> right);
+  free(root);
+}
 

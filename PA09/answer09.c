d485de4f8761732ca53ba229b280dd23110af8de
Yiwen Gong <gong32@ecegrid-thin5.ecn.purdue.edu>
2014-11-09 01:09:31 -0500

    passed the tester

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -6,14 +6,14 @@
 BusinessNode * create_node(char * stars, char * name, char * address)
 {
   BusinessNode * node;
-  node = malloc(sizeof(BussinessNode));
+  node = malloc(sizeof(BusinessNode));
   if(node == NULL)
   {
     return node;
   }
-  node -> stars = *stars;
-  node -> address = *address;
-  node -> name = *name;
+  node -> stars = stars;
+  node -> address = address;
+  node -> name = name;
   node -> left = NULL;
   node -> right = NULL;
   return(node);
@@ -23,9 +23,9 @@ BusinessNode * tree_insert(BusinessNode * node, BusinessNode * root)
 {
   if(root == NULL)
   {
-    return(create_node(node));
+    return(node);
   }
-  if((node -> name) <= (root -> name))
+  else if(strcmp((node -> name),(root -> name)) <= 0)
   {
     root -> left = tree_insert(node, root -> left); 
   }
@@ -36,20 +36,25 @@ BusinessNode * tree_insert(BusinessNode * node, BusinessNode * root)
   return root;
 }
 
-char * * explode(const char * str, const char * delims)
+char * * explode(char * str)
 {  
   int ind;
   int N = 0;
   int arrInd = 0; // this is the next position where we'll create a string
   int last = 0; // 1 + the last index we saw a delimiter. Init to 0.
-  for(ind = 0;ind<strlen(str);ind++)
+  if(str == NULL)
   {
-    if(strchr(delims, str[ind]) != NULL)
+    return NULL;
+  }
+  for(ind = 0;ind < strlen(str);ind++)
+  {
+    if(str[ind]=='\t')
     {
       N++;
     }
   }
   int arrLen = N+1; //if there is one delim, there should be two arrays in the string.
+  //printf("arrLen = %d",arrLen);
   char * * strArr = malloc((arrLen) * sizeof(char *));//create the return array
   if(N != 0)
   {
@@ -57,7 +62,7 @@ char * * explode(const char * str, const char * delims)
     {
       for(ind = 0; ind <strlen(str) ; ind++)
       {
-	if(strchr(delims, str[ind]) != NULL)
+	if(str[ind] == '\t')
 	{
 	  strArr[arrInd] = malloc(sizeof(char) * (ind-last+1));
 	  memcpy(strArr[arrInd],str+last,ind-last);
@@ -82,39 +87,66 @@ char * * explode(const char * str, const char * delims)
 
 BusinessNode * load_tree_from_file(char * filename)
 {
-  FILE * fptr = fopen(filename,'r');
-  BussinessNode * root = NULL;
-  while(!foef(fptr))
+  FILE * fptr = fopen(filename,"r");
+  if(fptr == NULL)
   {
-    char * ch;
-    int ind = 0;
-   do
-   {
-     ch[ind] = fgetc(fptr);
-     ind++;
-   }while(ch != '\n');
-   char **strArr = explode(ch,'\t');
-   BussinessNode * node = create_node(strArr[0],strArr[1],strArr[2]);
-   root = tree_insert(node, root);
+    return NULL;
   }
+  BusinessNode * root=NULL;
+  while(! feof(fptr))
+  {
+    char ch[250];
+    fgets(ch, 200, fptr);
+    char * * strArr = explode(ch);
+    //printf("%s %s %s\n",strArr[0],strArr[1],strArr[2]);
+    BusinessNode * node = create_node(strArr[0],strArr[1],strArr[2]);
+    //free(strArr[0]);
+    //free(strArr[1]);
+    //free(strArr[2]);
+    //free(ch);
+    free(strArr);
+    root = tree_insert(node, root);
+    //print_tree(root);
+  }
+  fclose(fptr);
   return root;
 }
      
 BusinessNode * tree_search_name(char * name, BusinessNode * root)
 {
+  BusinessNode * find = NULL;
+  if(root == NULL)
+  {
+    return NULL;
+  }
+  if(strcmp(name, root -> name) ==0)
+  {
+    return root;
+  }
+  if(strcmp(name, root -> name) < 0)
+  {
+    find = tree_search_name(name, root -> left);
+  }
+  if(strcmp(name, root -> name) > 0)
+  {
+    find = tree_search_name(name,root -> right);
+  }
+  return find;
+  /*
   if(root == NULL)
   {
     return NULL;
   }
   if(name == (root -> name))
   {
+    print_node(root);
     return root;
   }
-  if(name <= (root -> name))
+  if(name < (root -> name))
   {
     return tree_search_name(name, root -> left);
   }
-  return tree_search_name(name, root -> right);
+  return tree_search_name(name, root -> right);*/
 }
 
 
@@ -133,9 +165,9 @@ void print_tree(BusinessNode * tree)
   {
     return;
   }
-  printf("rate:%s name: %s address: %s\n",tree -> stars, tree -> name, tree -> address);
-  print_tree(printPreorder(tree -> left);
-  print_tree(printPreorder(tree -> right);
+  print_tree(tree -> left);
+  printf("rate:%s \nname: %s \naddress: %s \n",tree -> stars, tree -> name, tree -> address);
+  print_tree(tree -> right);
 }
 
 
@@ -146,7 +178,36 @@ void destroy_tree(BusinessNode * root)
     return;
   }
   destroy_tree(root -> left);
-  destory_tree(root -> right);
+  destroy_tree(root -> right);
+  free(root->stars);
+  free(root->name);
+  free(root->address);
   free(root);
 }
+/*
+int main(int argc, char * * argv)
+{
+   //BusinessNode * root = create_node("5.0", "random name", "random address");
+   //root->left = create_node("3.5", "another name", "another address");
+   //root->right = create_node("4.0", "yet another name", "some address");
+   //root->left->right = create_node("1.5", "name 3", "address 3");
+   //print_tree(root);
+   BusinessNode * root = load_tree_from_file("yelp_businesses.tsv");
+   BusinessNode * node = create_node(strdup("5.0"),strdup("test node"),strdup("no address"));
+   root = tree_insert(node,root);
+   print_tree(root);
+   //BusinessNode *node = tree_search_name("Eric Goldberg",tree);
+   //print_node(node);
+   //print_node(tree_search_name("another name",root));
+   //root = load_tree_from_file(argv[1]);
+  // print_tree(root);
+   return 0;
+}
+*/
+
+
+
+
+
+
 
